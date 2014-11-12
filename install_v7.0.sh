0177089e00a787c6d9ff5961ebee0f2ad7c8551b
nimusis <nimusis@run2u.kr>
2014-11-12 18:29:00 +0900

    add source

---
+++ install_v7.0.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+filename=".kwon_env_v7.0.tar.gz"
+bashfilename=$HOME/".bashrc"
+
+wget -O $filename "http://kwon.kr/vim_env/kwon_env_v7.0.tar.gz"
+
+gunzip -c $filename | tar -xf - --directory $HOME
+
+
+if grep -q '.kwon_env' $HOME/.bashrc; then
+	echo "kwon_env aliases already exist!"
+else
+	echo "" >> $HOME/.bashrc
+	echo "#aliases for kwon_env" >> $HOME/.bashrc
+	echo "alias kwon='source ~/.kwon_env/bashrc'" >> $HOME/.bashrc
+	echo "" >> $HOME/.bashrc
+fi
+
+rm -f $filename
+
+
